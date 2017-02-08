31367d69a0c4bf651be8e26d8af024645b784f0b
Steven Bergqvist <stevenbergqvist@gmail.com>
2017-02-08 13:57:13 +0100

    Added CSS successfully

---
+++ expresswebsite.js
@@ -0,0 +1,29 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser")
+
+//läsa data från POST body
+
+var MyParser = bodyParser.urlencoded();
+
+app.get("/", function (req,res){
+    //res.send("Hello from Express");
+    res.sendFile(__dirname + "/" +"index.html");
+})
+
+app.get("/products", function(req,res){
+    console.log("Got a request for products");
+    res.send("Listing all products");
+})
+
+app.post("/namehandler", MyParser, function(req,res){
+    console.log(req.body.firstname);
+    res.end("Hello "+req.body.firstname);
+})
+
+var server = app.listen(8088, function(){
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log("Express app listening at %s %s", host, port)
+})
